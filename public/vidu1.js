134a9dde56e8647fc7869bf3c486b35bd8ff035c
Admin <vanpho02@gmail.com>
2017-03-08 20:51:03 +0700

    component gia

--- public/vidu1.js
+++ public/vidu1.js
@@ -1,14 +1,27 @@
+class Gia extends React.Component {
+    render(){
+        return <p>{this.props.price}</p>
+    }
+}
+
 class KhoaHoc extends React.Component {
     render() {
-        return <a href="http://khoapham.vn">KhoaPham</a>;
+        let { ten, teacher } = this.props;
+        return (
+            <div>
+                <h3>{ten}</h3>
+                <p>{teacher}</p>
+            </div>
+        );
     }
 }
 
 ReactDOM.render(
     <div>
-        <KhoaHoc />
+        <KhoaHoc ten="NodeJS" teacher="Khoa Pham"/>
         <br/>
-        <KhoaHoc />
+        <Gia price="5000"/>
+        <KhoaHoc ten="ReactJS" teacher="Pho"/>
     </div>,
     document.getElementById('root')
 );
