10cb2aab03f4b043ec1ad6ee42f6c0a70bf72c35
Admin <vanpho02@gmail.com>
2017-03-10 20:25:42 +0700

    add and sub

--- public/vidu3.js
+++ public/vidu3.js
@@ -8,11 +8,36 @@ class Box extends React.Component {
     }
     render() {
         return( 
-            <button onClick={this.add.bind(this)}>{this.state.value}</button>
+            <button 
+                onClick={this.add.bind(this)}
+            >{this.state.value}</button>
         );
     }
 }
 
-ReactDOM.render(<Box />, 
-    document.getElementById('root'));
+class Count extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 0 };
+    }
+    add() {
+        const { value } = this.state;
+        this.setState({ value:  value + 1});
+    }
+    sub() {
+        const { value } = this.state;
+        this.setState({ value:  value - 1});
+    }
+    render() {
+        const { value } = this.state;
+        return (
+            <p>
+                <button onClick={this.add.bind(this)}> + </button>
+                <h3>{value}</h3>
+                <button onClick={this.sub.bind(this)}> - </button>
+            </p>
+        )
+    }
+}
 
+ReactDOM.render(<Count />, document.getElementById('root'));
