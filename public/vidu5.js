8caf67ef30006c00d08edd20171b714651ef582c
Admin <vanpho02@gmail.com>
2017-03-13 20:28:55 +0700

    list of elements

--- public/vidu5.js
+++ public/vidu5.js
@@ -1,33 +1,42 @@
 //Note 3 props: subject content important <h3> <p> red 
+const getClass = isImportant => isImportant ? "red" : "green";
+const getHtml = important => important ? null : <button>Xoá</button>;
 
 class Note extends React.Component {
     render() {
         const { subject, content, important } = this.props;
-        const xhtml = important ? null : <button>Xoá</button>;
-        const getClass = isImportant => important ? "red" : "green";
-        
         return (
             <div>
                 <h3 className={getClass(important)}>{subject}</h3>
                 <p>{content}</p>
-                {xhtml}
+                {getHtml(important)}
+            </div>
+        );
+    }
+}
+
+class List extends React.Component {
+    state = { mang: [
+        { id: 1, subject: 'Hoc Tap', content: 'Lam bai tap ve nha', important: false},
+        { id: 2, subject: 'Hoc Tap', content: 'Nop do an cuoi khoa', important: true},
+        { id: 3, subject: 'An Choi', content: 'Di choi 8/3', important: false}
+    ]}
+
+    render() {
+        let arrayEle = this.state.mang.map( e => <Note 
+            subject={e.subject}
+            content={e.content}
+            important={e.important}
+        /> );
+        return (
+            <div>
+                { arrayEle }
             </div>
         );
     }
 }
 
 ReactDOM.render(
-    <div>
-        <Note 
-            subject="Hoc tap"
-            content="Nho lam bai tap ve nha"
-            important={false}
-        />
-        <Note 
-            subject="An Choi"
-            content="Nho lam bai tap ve nha"
-            important={true}
-        />
-    </div>,
+    <List />,
     document.getElementById('root')
 );
